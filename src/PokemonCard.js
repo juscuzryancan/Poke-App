cb44bf3c4e48b7c0a136f1e2474001f86307b8c7
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-01-07 18:18:38 -0800

    Added height and weight to divs

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -3,6 +3,8 @@ import { getData } from './api';
 import { Card } from '@material-ui/core';
 
 const PokemonCard = ({name, url}) => {
+    //The set function within useState is asynchronous
+    //The state variable doesn't immediately change
     const [pokemonData, setPokemonData] = useState({});
 
     const handlePokemonData = useCallback(async () => {
@@ -23,6 +25,9 @@ const PokemonCard = ({name, url}) => {
             <section>
                 {name}
             </section>
+            <div>ID: {pokemonData.id}</div>
+            <div>Height: {pokemonData.height}</div>
+            <div>Weight: {pokemonData.weight}</div>
         </Card>
     );
 
